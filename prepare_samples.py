1f2b7eb3c1218423423af66956dd9224aba51d34
Alexandre Bigot <abigot@sbgat402.in2p3.fr>
2023-10-18 09:43:23 +0200

    Add MegaLinter config + MegaLinter fixes applied to preparation and training macros

--- prepare_samples.py
+++ prepare_samples.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 
+# Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+# See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+# All rights not expressly granted are reserved.
+
+# This software is distributed under the terms of the GNU General Public
+# License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+
+# In applying this license CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
 """
 file: prepare_samples.py
 brief: script for the preparation of the samples for the training
        of ML models to be used in PWGHF/D2H
 note: inspired by EventFiltering/PWGHF/Macros/prepare_samples.py
-usage: python3 prepare_samples.py CONFIG 
+usage: python3 prepare_samples.py CONFIG
 author: Alexandre Bigot <alexandre.bigot@cern.ch>, Strasbourg University
 """
 
 import argparse
 import os
 
-import numpy as np
-import uproot
-import yaml
-from alive_progress import alive_bar
+import numpy as np  # pylint: disable=import-error
+import uproot  # pylint: disable=import-error
+import yaml  # pylint: disable=import-error
+from alive_progress import alive_bar  # pylint: disable=import-error
 
 MAX_FILES = 1000
+
 
 # pylint: disable=too-many-locals
 def combine_tpc_or_tof(df, mass_hypos, n_prongs):
@@ -36,7 +48,7 @@ def combine_tpc_or_tof(df, mass_hypos, n_prongs):
     """
 
     nsigma_tolerance = 0.1
-    nsigma_dummy = -999. + nsigma_tolerance
+    nsigma_dummy = -999.0 + nsigma_tolerance
     logic = "or"
 
     nsigma_cols = []
@@ -54,8 +66,7 @@ def combine_tpc_or_tof(df, mass_hypos, n_prongs):
             cols_to_remove.append(nsigma_tof_col)
             nsigma_combined_cols[suffix] = []
             # apply TPC OR TOF logic
-            df.query(f"{nsigma_tpc_col} > {nsigma_dummy} {logic} {nsigma_tof_col} > {nsigma_dummy}",
-                     inplace=True)
+            df.query(f"{nsigma_tpc_col} > {nsigma_dummy} {logic} {nsigma_tof_col} > {nsigma_dummy}", inplace=True)
 
     for row in list(df.index):
         for mass_hypo in mass_hypos:
@@ -68,12 +79,13 @@ def combine_tpc_or_tof(df, mass_hypos, n_prongs):
                 nsigma_tof = df.at[row, nsigma_tof_col]
 
                 if nsigma_tpc > nsigma_dummy:
-                    if nsigma_tof > nsigma_dummy: # TPC and TOF
+                    if nsigma_tof > nsigma_dummy:  # TPC and TOF
                         nsigma_combined_cols[suffix].append(
-                            np.sqrt(0.5*(nsigma_tpc*nsigma_tpc + nsigma_tof*nsigma_tof)))
-                    else: # only TPC
+                            np.sqrt(0.5 * (nsigma_tpc * nsigma_tpc + nsigma_tof * nsigma_tof))
+                        )
+                    else:  # only TPC
                         nsigma_combined_cols[suffix].append(np.abs(nsigma_tpc))
-                else: # only TOF
+                else:  # only TOF
                     if nsigma_tof > nsigma_dummy:
                         nsigma_combined_cols[suffix].append(np.abs(nsigma_tof))
 
@@ -86,6 +98,7 @@ def combine_tpc_or_tof(df, mass_hypos, n_prongs):
             df.insert(0, "fNSigComb" + suffix, nsigma_combined_cols[suffix])
 
     return df
+
 
 def divide_df_for_origin(df, cols_to_remove=None):
     """
@@ -104,7 +117,7 @@ def divide_df_for_origin(df, cols_to_remove=None):
     """
 
     if cols_to_remove is None:
-        cols_to_remove = ['fOriginMcRec']
+        cols_to_remove = ["fOriginMcRec"]
 
     df_bkg = df.query("fOriginMcRec == 0")
     df_prompt = df.query("fOriginMcRec == 1")
@@ -130,26 +143,26 @@ def main(cfg):
     """
 
     # import configurables
-    channel = cfg['channel']
-    labels = cfg['labels']
-    input_files = cfg['prepare_samples']['input']['files']
-    input_tree_name = cfg['prepare_samples']['input']['tree_name']
-    combine_pid_vars = cfg['prepare_samples']['pid']['combine_vars']
-    downscale_bkg = cfg['prepare_samples']['downscale_bkg']
-    inv_mass_sidebands = cfg['prepare_samples']['filt_bkg_mass']
-    seed_split = cfg['seed_split']
-    force = cfg['prepare_samples']['output']['force']
+    channel = cfg["channel"]
+    labels = cfg["labels"]
+    input_files = cfg["prepare_samples"]["input"]["files"]
+    input_tree_name = cfg["prepare_samples"]["input"]["tree_name"]
+    combine_pid_vars = cfg["prepare_samples"]["pid"]["combine_vars"]
+    downscale_bkg = cfg["prepare_samples"]["downscale_bkg"]
+    inv_mass_sidebands = cfg["prepare_samples"]["filt_bkg_mass"]
+    seed_split = cfg["seed_split"]
+    force = cfg["prepare_samples"]["output"]["force"]
 
+    mass_hypos, n_prongs = None, None
     if combine_pid_vars:
-        mass_hypos = cfg['prepare_samples']['pid']['mass_hypos']
-        n_prongs = cfg['prepare_samples']['pid']['n_prongs']
+        mass_hypos = cfg["prepare_samples"]["pid"]["mass_hypos"]
+        n_prongs = cfg["prepare_samples"]["pid"]["n_prongs"]
 
     df_tot = None
 
     with alive_bar(len(input_files[:MAX_FILES])) as bar_alive:
         for file in input_files[:MAX_FILES]:
-            print(f"\033[32mExtracting dataframes from input "
-                  f"{file}\033[0m")
+            print(f"\033[32mExtracting dataframes from input " f"{file}\033[0m")
 
             file_root = uproot.open(file)
             indir = os.path.split(file)[0]
@@ -172,21 +185,13 @@ def main(cfg):
 
                 df_bkg, df_prompt, df_nonprompt = divide_df_for_origin(df_tot)
 
-                df_bkg = df_bkg.sample(
-                    frac=downscale_bkg, random_state=seed_split)
-                df_bkg.query(inv_mass_sidebands, inplace=True) # select bkg candidates
-                df_bkg.to_parquet(
-                    os.path.join(indir, f"{labels[0]}_{channel}.parquet.gzip"),
-                    compression="gzip"
-                )
-                df_prompt.to_parquet(
-                    os.path.join(indir, f"{labels[1]}_{channel}.parquet.gzip"),
-                    compression="gzip"
-                )
+                df_bkg = df_bkg.sample(frac=downscale_bkg, random_state=seed_split)
+                df_bkg.query(inv_mass_sidebands, inplace=True)  # select bkg candidates
+                df_bkg.to_parquet(os.path.join(indir, f"{labels[0]}_{channel}.parquet.gzip"), compression="gzip")
+                df_prompt.to_parquet(os.path.join(indir, f"{labels[1]}_{channel}.parquet.gzip"), compression="gzip")
                 if not df_nonprompt.empty:
                     df_nonprompt.to_parquet(
-                        os.path.join(indir, f"{labels[2]}_{channel}.parquet.gzip"),
-                        compression="gzip"
+                        os.path.join(indir, f"{labels[2]}_{channel}.parquet.gzip"), compression="gzip"
                     )
 
                 df_tot = None
@@ -195,14 +200,13 @@ def main(cfg):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Arguments')
-    parser.add_argument('config', metavar='text', default='config.yml',
-                        help='config file name for ml')
+    parser = argparse.ArgumentParser(description="Arguments")
+    parser.add_argument("config", metavar="text", default="config.yml", help="config file name for ml")
     args = parser.parse_args()
 
-    print('Loading analysis configuration: ...', end='\r')
+    print("Loading analysis configuration: ...", end="\r")
     with open(args.config, "r", encoding="utf-8") as yml_cfg:
         config = yaml.load(yml_cfg, yaml.FullLoader)
-    print('Loading analysis configuration: Done!')
+    print("Loading analysis configuration: Done!")
 
     main(config)
